76b8f9bfb0194b20dea4b61b0d83a1dec4bcf276
Lucas Nouguier <lucas.nouguier@protonmail.com>
2022-09-15 11:22:55 +0200

    Fucking missing export

--- src/routes/roots.js
+++ src/routes/roots.js
@@ -2,6 +2,7 @@ const baseURL = "https://bde-api.cluster-ig3.igpolytech.fr";
 
 export const clubs = baseURL + "/api/clubs/";
 export const events = baseURL + "/api/events/";
+export const goodies = baseURL + "/api/goodies/";
 export const partners = baseURL + "/api/partners/";
 export const promos = baseURL + "/api/promos/";
 export const users = baseURL + "/api/users/";
