789d220161a67f2f06035723ca8bce73fcbf9490
RedbeanGit <dubois.julien.mail@gmail.com>
2022-09-18 13:43:05 +0200

    feat: use env variables for api url
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

--- src/routes/roots.js
+++ src/routes/roots.js
@@ -1,11 +1,11 @@
-const baseURL = "https://bde-api.cluster-ig3.igpolytech.fr";
+const baseURL = process.env.REACT_APP_API_URL;
 
-export const clubs = baseURL + "/api/clubs/";
-export const events = baseURL + "/api/events/";
-export const goodies = baseURL + "/api/goodies/";
-export const partners = baseURL + "/api/partners/";
-export const promos = baseURL + "/api/promos/";
-export const users = baseURL + "/api/users/";
-export const roles = baseURL + "/api/roles/";
-export const signup = baseURL + "/api/signup/";
-export const signin = baseURL + "/api/signin/";
+export const clubs = baseURL + '/api/clubs/';
+export const events = baseURL + '/api/events/';
+export const goodies = baseURL + '/api/goodies/';
+export const partners = baseURL + '/api/partners/';
+export const promos = baseURL + '/api/promos/';
+export const users = baseURL + '/api/users/';
+export const roles = baseURL + '/api/roles/';
+export const signup = baseURL + '/api/signup/';
+export const signin = baseURL + '/api/signin/';
